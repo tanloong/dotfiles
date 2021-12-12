97c0b46ef7cce3a285da46b2e59cdbd949dbb64d
Tan Long <2638000368@qq.com>
2021-12-12 19:05:20 +0800

    link.sh: a shorter version
    ls

--- link.sh
+++ link.sh
@@ -2,27 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t '
 
+# This script makes symbolic links between cofigure files and their
+# corresponding destination files. Note that it will remove existing
+# destination files.
+
 cur_path=$(cd $(dirname $0) && pwd)
 
-#############
-### FUNCTIONS
-#############
-
-link() {
-    if [ ! -L "$2" ]; then
-        rm -f "$2" >/dev/null 2>&1 || :
-        sudo ln -s "$1" "$2"
-        echo "linked $2"
-    fi
-}
-
-#############
-### MAIN
-#############
 # 跳过 .git 文件夹，.gitignore，$cur_path/ 下非点号开头的文件
 files=$(find "$cur_path" -type f | grep -vE "(\.git\/|\.gitignore|$cur_path/[^\.])")
 for filename in $files; do
-    target="$HOME/"${filename/#*dotfiles\/}
-    mkdir -p ${target%/*} || :
-    link "$filename" "$target"
+    destination="$HOME/"${filename/#*dotfiles\/}
+    mkdir -p ${destination%/*} || :
+    ln --verbose --force --symbolic "$filename" "$destination"
 done
