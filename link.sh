b08eacd4a97db0ae1d50feaccb832f70ca4c9792
Tan Long <tanloong@foxmail.com>
2022-11-04 20:58:02 +0800

    style

--- link.sh
+++ link.sh
@@ -9,9 +9,9 @@ IFS=$'\n\t '
 cur_path=$(cd $(dirname $0) && pwd)
 
 # 跳过 .git 文件夹，.gitignore，$cur_path/ 下非点号开头的文件
-files=$(find "$cur_path" -type f | grep -vE "(\.git\/|\.gitignore|$cur_path/[^\.])")
+files=$(find "$cur_path" -type f | grep -vE "(\.git/|\.gitignore|$cur_path/[^\.])")
 for filename in $files; do
     destination="$HOME/"${filename/#*dotfiles\/}
-    mkdir -p ${destination%/*} || :
+    mkdir -p "${destination%/*}" || :
     ln --verbose --force --symbolic "$filename" "$destination"
 done
