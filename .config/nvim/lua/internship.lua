da475c4effd4a3e5c8e9d3a045f6f2a80fb612f3
Tan Long <tanloong@foxmail.com>
2023-08-04 16:48:25 +0800

    update

--- .config/nvim/lua/internship.lua
+++ .config/nvim/lua/internship.lua
@@ -1,37 +1,46 @@
 #!/usr/bin/env lua
--- 用于上外实习
--- 2022年9月-12月
 
-local keyset = vim.keymap.set
-
-M = {}
-function M.strip(s)
-    return string.gsub(s, '%s+$', '')
-end
-
-function M.intrn_join()
-    -- 将光标所在pair的中文或英文行合并到上一个pair对应中文或英文行的末尾
-    local lineno = vim.fn.line(".")
+local AppendLineToThreeLinesAbove = function(lineno)
+    --[[ 将行所在pair的中文或英文行合并到上一个pair对应中文或英文行的末尾 ]]
     local lineno_minus3 = lineno - 3
     local lineno_minus1 = lineno - 1
-    local line = vim.fn.getline(".")
+    local line = vim.fn.getline(lineno)
     local line_minus3 = vim.fn.getline(lineno_minus3)
     local line_minus1 = vim.fn.getline(lineno_minus1)
 
     local sep = " "
     if (line_minus1 == "" or line_minus3 == "") then sep = "" end
-    vim.fn.setline(lineno_minus3, M.strip(line_minus3) .. sep .. line)
+    vim.fn.setline(lineno_minus3, string.gsub(line_minus3, '%s+$', '') .. sep .. line)
     vim.fn.setline(lineno, '')
 end
 
-local map_intern = function()
-    keyset('n', 'K', '3k', { buffer = true })
-    keyset('n', 'J', '3j', { buffer = true })
+local MergeLines = function()
+    local lineno = vim.fn.line(".")
+    if lineno <= 3 then return end
 
-    keyset('n', 'M', '<Cmd>silent!! intrn-align-backup.sh %<CR><Cmd>lua M.intrn_join()<CR>', { buffer = true, nowait=true })
-    keyset('n', 'd', '<Cmd>write<CR><Cmd>silent!! intrn-align-update.sh %<CR>', { buffer = true, nowait=true })
-    keyset('n', ',', 'Md', { remap = true }, { buffer = true, nowait=true })
-    keyset('n', 'r', '<Cmd>let pos=getcurpos()[1:] | %d | 0r %.bak | call cursor(pos)<CR>', { buffer = true, nowait=true })
+    local last_lineno = vim.fn.line("$")
+
+    while lineno <= last_lineno do
+        AppendLineToThreeLinesAbove(lineno)
+        lineno = lineno + 3
+    end
+
+    local buf = vim.api.nvim_get_current_buf()
+    while string.match(vim.fn.getline(last_lineno), "^%s*$") do
+        vim.fn.deletebufline(buf, last_lineno)
+        last_lineno = last_lineno - 1
+    end
+end
+
+local map_intern = function()
+    local mappings = {
+        { mode = 'n', from = ',', to = MergeLines },
+        { mode = 'n', from = 'K', to = '3kzz' },
+        { mode = 'n', from = 'J', to = '3jzz' }
+    }
+    for _, mapping in ipairs(mappings) do
+        vim.keymap.set(mapping.mode, mapping.from, mapping.to, { buffer = true, nowait = true })
+    end
 end
 
 vim.api.nvim_create_autocmd("BufEnter", { pattern = "*interlaced*.txt", callback = map_intern })
