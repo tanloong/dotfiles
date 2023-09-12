46910126165a1e3609a7d55c8bde27b9b28afaab
Tan Long <tanloong@foxmail.com>
2023-09-13 00:54:43 +0800

    update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,28 +1,30 @@
 #!/usr/bin/env lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-vim.cmd([[
-    augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-    augroup end
-]])
-
-local packer = require('packer')
-packer.init {
-    display = {
-        open_fn = function()
-            return require("packer.util").float({ border = 'single' })
+local plugin_specs = {
+    {
+        'https://gitee.com/tanloong/auto-save.nvim.git',
+        cond = function()
+            local bufnr = vim.api.nvim_get_current_buf()
+            return not string.find(vim.api.nvim_buf_get_name(bufnr), "interlaced.*%.txt$")
+        end,
+        config = function()
+            require("plugin_config.autosave")
         end
-    }
-}
-
-return packer.startup(function(use)
-    -- Packer can manage itself
-    use { 'https://gitee.com/nvim-plugin/packer.nvim' }
-    use { 'https://github.com/neoclide/coc.nvim.git',
+    },
+    {
+        'https://github.com/neoclide/coc.nvim.git',
         branch = 'release',
         event = { 'InsertEnter', 'CursorHold' },
         -- don't load coc.nvim on "interlaced" filetype
@@ -30,73 +32,105 @@ return packer.startup(function(use)
             local bufnr = vim.api.nvim_get_current_buf()
             return not string.find(vim.api.nvim_buf_get_name(bufnr), "interlaced.*%.txt$")
         end,
-        config = [[require('plugin_config.coc')]] }
-    use { 'https://gitee.com/tanloong/auto-save.nvim.git',
-        cond = function()
-            local bufnr = vim.api.nvim_get_current_buf()
-            return not string.find(vim.api.nvim_buf_get_name(bufnr), "interlaced.*%.txt$")
-        end,
-        config = [[require('plugin_config.autosave')]] }
-    use { 'https://gitee.com/tanloong/vim-surround.git',
-        config = [[vim.keymap.set('x', 's', '<Plug>VSurround')]] }
-    use { 'https://github.com/numToStr/Comment.nvim',
-        config = [[require('plugin_config.comment')]] }
-    use { 'https://gitee.com/lovealone72/vim-markdown-toc.git',
+        config = function() require('plugin_config.coc') end
+    },
+    {
+        'https://gitee.com/tanloong/vim-surround.git',
+        config = function() vim.keymap.set('x', 's', '<Plug>VSurround') end
+    },
+    {
+        'https://github.com/numToStr/Comment.nvim',
+        config = function() require('plugin_config.comment') end
+    },
+    {
+        'https://gitee.com/lovealone72/vim-markdown-toc.git',
         ft = { 'markdown' },
-        config = [[require('plugin_config.vim_markdown_toc')]] }
-    use { 'https://gitee.com/yaozhijin/vim-table-mode.git',
+        config = function() require('plugin_config.vim_markdown_toc') end
+    },
+    {
+        'https://gitee.com/yaozhijin/vim-table-mode.git',
         ft = { 'markdown' },
-        config = [[require('plugin_config.vim_table_mode')]] }
-    use { 'https://gitee.com/tanloong/vim-slime',
-        config = [[require('plugin_config.vim_slime')]] }
-    use { 'https://gitee.com/mirrors/vimtex.git',
+        config = function() require('plugin_config.vim_table_mode') end
+    },
+    {
+        'https://gitee.com/tanloong/vim-slime',
+        config = function() require('plugin_config.vim_slime') end
+    },
+    {
+        'https://gitee.com/mirrors/vimtex.git',
         -- cmd = { 'VimtexCompile' },
-        config = [[require('plugin_config.vimtex')]] }
-    use { 'https://gitee.com/tanloong/hop.nvim',
+        config = function() require('plugin_config.vimtex') end
+    },
+    {
+        'https://gitee.com/tanloong/hop.nvim',
         branch = 'match-mappings',
-        config = [[require('plugin_config.hop')]] }
-    use { 'nvim-treesitter/nvim-treesitter',
+        config = function() require('plugin_config.hop') end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
-        config = [[require('plugin_config.nvim_treesitter')]],
-        event = 'CursorHold' }
-    use({
+        config = function() require('plugin_config.nvim_treesitter') end,
+        event = 'CursorHold'
+    },
+    {
         "nvim-treesitter/nvim-treesitter-textobjects",
-        after = "nvim-treesitter",
-        requires = "nvim-treesitter/nvim-treesitter",
-        config = [[require('plugin_config.nvim_treesitter_textobjects')]],
-    })
-    use { "https://github.com/SUSTech-data/wildfire.nvim",
-        after = "nvim-treesitter",
-        requires = "nvim-treesitter/nvim-treesitter",
-        config = [[require('plugin_config.wildfire')]],
-    }
-    use { 'lukas-reineke/indent-blankline.nvim',
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = function() require('plugin_config.nvim_treesitter_textobjects') end,
+    },
+    {
+        "https://github.com/SUSTech-data/wildfire.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = function() require('plugin_config.wildfire') end,
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim',
         event = "CursorHold",
-        config = [[require('plugin_config.indent_blankline')]] }
-    use { 'https://gitee.com/tanloong/nvim-align.git',
-        event = "CursorHold" }
-    use { "akinsho/toggleterm.nvim",
+        config = function() require('plugin_config.indent_blankline') end
+    },
+    {
+        'https://gitee.com/tanloong/nvim-align.git',
+        event = "CursorHold"
+    },
+    {
+        "akinsho/toggleterm.nvim",
         event = "CursorHold",
-        config = [[require('plugin_config.toggleterm')]] }
-    use {
+        config = function() require('plugin_config.toggleterm') end
+    },
+    {
         "lmburns/lf.nvim",
-        config = [[require('plugin_config.lf_nvim')]],
-        requires = { "toggleterm.nvim" } }
-    use {
+        config = function() require('plugin_config.lf_nvim') end,
+        dependencies = { "toggleterm.nvim" }
+    },
+    {
         "nat-418/boole.nvim",
         event = "CursorHold",
-        config = [[require('plugin_config.boole_nvim')]],
-    }
-    use { 'nvimdev/hlsearch.nvim', event = 'BufRead', config = function()
-        require('hlsearch').setup()
-    end
-    }
-    use { '~/projects/interlaced.nvim',
+        config = function() require('plugin_config.boole_nvim') end,
+    },
+    {
+        'nvimdev/hlsearch.nvim',
+        event = 'BufRead',
+        config = function()
+            require('hlsearch').setup()
+        end
+    },
+    {
+        '~/projects/interlaced.nvim',
         cond = function()
             local bufnr = vim.api.nvim_get_current_buf()
             return string.find(vim.api.nvim_buf_get_name(bufnr), "interlaced.*%.txt$") and true or false
         end,
-        config = [[require('plugin_config.interlaced_nvim')]],
-    }
-    use { 'kaarmu/typst.vim', ft = { 'typst' } }
-end)
+        config = function() require('plugin_config.interlaced_nvim') end,
+    },
+    { 'kaarmu/typst.vim', ft = { 'typst' } },
+}
+
+-- configuration for lazy itself.
+local lazy_opts = {
+    ui = {
+        border = "single",
+        title = "Plugin Manager",
+        title_pos = "center",
+    },
+}
+
+require("lazy").setup(plugin_specs, lazy_opts)
