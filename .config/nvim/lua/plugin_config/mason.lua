147e86b652c93e962e3341e9b90cf54b85d2b900
Tan Long <tanloong@foxmail.com>
2023-12-13 22:59:09 +0800

    add lsp and cmp plugins

---
+++ .config/nvim/lua/plugin_config/mason.lua
@@ -0,0 +1,11 @@
+#!/usr/bin/env lua
+
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
