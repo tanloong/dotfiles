f2da5b364e3c5fe719d78e8368e13d82e4e2c5e5
Tan Long <tanloong@foxmail.com>
2024-02-16 17:05:00 +0800

    update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -304,15 +304,22 @@ local plugin_specs = {
         tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            local builtin = require('telescope.builtin')
             require("telescope").setup {
                 defaults = {
-                    mappings = { i = { ["<C-u>"] = false }, },
+                    preview = false,
+                    mappings = {
+                        i = {
+                            ["<c-u>"] = false,
+                            ["<esc>"] = require('telescope.actions').close,
+                        },
+                    },
                 }
             }
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>b', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+            keyset('n', '<leader>b', builtin.find_files, {})
+            keyset('n', '<leader>g', builtin.live_grep, {})
+            keyset('n', '<leader>h', builtin.help_tags, {})
+            keyset('n', '<c-/>', builtin.current_buffer_fuzzy_find, {})
 
             vim.cmd(
                 [[
