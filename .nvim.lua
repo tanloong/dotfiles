da6affe6d429429ed1675060ba907e67f3e0c29c
Tan Long <tanloong@foxmail.com>
2024-11-11 22:46:56 +0800

    update

--- .nvim.lua
+++ .nvim.lua
@@ -2,14 +2,15 @@
 
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
+local vim_fs = vim.fs
 
-local curr_path = debug.getinfo(1, "S").source
-curr_path = curr_path:sub(2, curr_path:find("[^/\\]*$") - 1) -- 截取出目录
+local script_parent = vim_fs.dirname(vim.fn.expand("%:p"))
+local fcitx5_path = vim_fs.joinpath(script_parent, ".local", "share", "fcitx5")
 
 autocmd("BufWritePost", {
-    pattern = curr_path .. ".local/share/fcitx5/table/*.txt",
-    group = augroup("build_fcitx5_table", { clear = true }),
-    callback = function()
-      os.execute("bash " .. curr_path .. "./build_fcitx5_table.sh > /dev/null 2>&1")
-    end
+  pattern = { fcitx5_path .. "/table/*.txt", fcitx5_path .. "/inputmethod/*.conf" },
+  group = augroup("build_fcitx5_table", { clear = true }),
+  callback = function()
+    os.execute("bash " .. vim_fs.joinpath(script_parent, "build_fcitx5_table.sh") .. " > /dev/null 2>&1")
+  end
 })
--- build_fcitx5_table.sh
+++ build_fcitx5_table.sh
@@ -4,9 +4,12 @@ IFS=$'\n\t'
 
 curr_path=$(cd $(dirname $0) && pwd)
 
-idir="$curr_path"/.local/share/fcitx5/table
-should_restart_fcitx5=0
-for file in "$idir"/*.txt; do
+table_dir="$curr_path"/.local/share/fcitx5/table
+conf_dir="$curr_path"/.local/share/fcitx5/inputmethod
+should_restart=0
+
+# Check for table_dir
+for file in "$table_dir"/*.txt; do
   destination="$HOME/"${file/#*dotfiles\/}
   destination="${destination/.txt/.dict}"
   sha1sum_file="$file.sha1sum"
@@ -15,13 +18,25 @@ for file in "$idir"/*.txt; do
   if [ ! -f "$destination" ] || [ ! -f "$sha1sum_file" ] || [ "$(cat "$sha1sum_file")" != "$file_sha1sum" ]; then
     libime_tabledict "$file" "$destination"
     echo "$file_sha1sum" > "$sha1sum_file"
-    should_restart_fcitx5=1
+    should_restart=1
     echo "[Fcitx5] Generated $destination and $sha1sum_file"
   fi
 done
 
-if [ "$should_restart_fcitx5" -eq 1 ]; then
+# Check for conf_dir
+for file in "$conf_dir"/*.conf; do
+  sha1sum_file="$file.sha1sum"
+  file_sha1sum=$(sha1sum "$file" | awk '{print $1}')
+
+  if [ ! -f "$sha1sum_file" ] || [ "$(cat "$sha1sum_file")" != "$file_sha1sum" ]; then
+    echo "$file_sha1sum" > "$sha1sum_file"
+    should_restart=1
+  fi
+done
+
+# Restart or reload fcitx5
+if [ "$should_restart" -eq 1 ]; then
   fcitx5-remote -e # request fcitx5 to exit
   fcitx5-remote > /dev/null # launch fcitx5
-  echo "[Fcitx5] Restarted fcitx5"
+  echo "[Fcitx5] Restarted"
 fi
