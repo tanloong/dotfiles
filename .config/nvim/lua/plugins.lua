611380019edbc9bc4fd56b12309d45059c0b2231
Tan Long <tanloong@foxmail.com>
2024-12-25 15:15:24 +0800

    update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -395,8 +395,22 @@ local plugin_specs = {
   {
     "neovim/nvim-lspconfig",
     enabled = true,
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
     event = { "BufReadPre", "BufNewFile" },
-    config = function() require("plugin_config.lsp") end,
+    -- config = function() require("plugin_config.lsp") end,
+    config = function() require("lspconfig").lua_ls.setup {} end,
   },
   {
     "hrsh7th/nvim-cmp",
