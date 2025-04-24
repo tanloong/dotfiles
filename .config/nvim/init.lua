71844ed3387e3454ca83a9848082e45fed87f046
Tan Long <tanloong@foxmail.com>
2025-04-24 14:47:43 +0800

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
 require('basics')
 require('keymaps')
 require('plugins')
+vim.lsp.enable({"lua", "python"})
---
+++ .config/nvim/lsp/python.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { 'pyright' },
+  filetypes = { 'python' },
+  root_markers = { '.gitignore', 'requirements.txt', 'pyproject.toml', 'Makefile', 'README.md' },
+}
