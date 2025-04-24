9ff228654a9b615e275af8a9392b2c40fd5312da
Tan Long <tanloong@foxmail.com>
2025-04-24 13:48:02 +0800

    update

---
+++ .config/nvim/lsp/luals.lua
@@ -0,0 +1,19 @@
+return {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.gitignore', '.luarc.json', '.luarc.jsonc' },
+  -- Specific settings to send to the server. The schema for this is
+  -- defined by the server. For example the schema for lua-language-server
+  -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+-- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+},
+    }
+  }
+}
