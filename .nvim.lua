010286ae77bb37399d9da74c3b880719aa247770
Tan Long <tanloong@foxmail.com>
2025-06-14 14:00:06 +0800

    update

--- .nvim.lua
+++ .nvim.lua
@@ -1,8 +1,10 @@
 #!/usr/bin/env lua
 
+local vim_fs = vim.fs
+if vim_fs.has("win32") == 1 then return end
+
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
-local vim_fs = vim.fs
 
 local script_parent = debug.getinfo(1, "S").source:sub(2, -1) -- 获取此脚本所在路径并去掉其开头的@
 script_parent = vim_fs.dirname(script_parent)
