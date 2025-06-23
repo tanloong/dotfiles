14af35ca27814f8580384dc983e29cfcecde7322
Tan Long <tanloong@foxmail.com>
2025-06-23 13:11:29 +0800

    update

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -4,3 +4,6 @@ Set-Alias -Name c -Value clear
 Set-Alias -Name pt -Value python
 Set-Alias -Name v -Value nvim
 Set-Alias -Name g -Value git
+
+# zoxide
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
