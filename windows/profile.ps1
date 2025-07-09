508db4f51f14c604a711b323a15cc21a542249cb
Tan Long <tanloong@foxmail.com>
2025-07-09 13:42:42 +0800

    update

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -5,5 +5,20 @@ Set-Alias -Name pt -Value python
 Set-Alias -Name v -Value nvim
 Set-Alias -Name g -Value git
 
+Set-PSReadLineOption -Colors @{
+    Command = 'Green'           # 命令（如 Get-Process）
+    Parameter = 'Magenta'      # 参数（如 -Name）
+    String = 'Yellow'          # 字符串
+    Number = 'Blue'            # 数字
+    Operator = 'Gray'      # 操作符（如 |、&&）
+    Variable = 'DarkYellow'    # 变量（如 $var）
+    Type = 'Cyan'             # 类型（如 [int]）
+    Comment = 'DarkGray'      # 注释
+}
+
+Set-PSReadLineKeyHandler -Key Ctrl+W -Function BackwardDeleteWord
+Set-PSReadLineKeyHandler -Key Ctrl+D -Function DeleteCharOrExit
+
 # zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
