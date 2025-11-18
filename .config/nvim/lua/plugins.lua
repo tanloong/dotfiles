c8897b0cf5c4230045e18126110989bf6ede7104
Tan Long <tanloong@foxmail.com>
2025-11-19 00:13:07 +0800

    update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -463,6 +463,34 @@ local plugin_specs = {
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {},
   },
+{ 'nvim-mini/mini.files', cond = not vim.g.vscode, version = false, config = function() 
+  require("mini.files").setup()
+  map("n", "tt", "<Cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>")
+  end },
+{
+  "obsidian-nvim/obsidian.nvim",
+  cond = not vim.g.vscode,
+  version = "*", -- recommended, use latest release instead of latest commit
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "C:/Users/Administrator/Desktop/Obsidian Vault",
+      },
+    },
+  },
+},
   {
     "luozhiya/fittencode.nvim",
     cond = not vim.g.vscode,
