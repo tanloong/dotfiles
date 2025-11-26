115be3c9c6a7583bc7ec36f4da886e65329d13e1
Tan Long <tanloong@foxmail.com>
2025-11-26 18:15:52 +0800

    u

--- .config/nvim/plugin/unload.lua
+++ .config/nvim/plugin/unload.lua
@@ -16,7 +16,7 @@ g.loaded_2html_plugin = 1
 g.loaded_logiPat = 1
 g.loaded_rrhelper = 1
 -- g.loaded_netrwPlugin = 1
-g.loaded_matchparen = 1
+-- g.loaded_matchparen = 1
 g.loaded_matchit = 1
 
 g.python_host_skip_check = 1
